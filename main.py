767705e1b100d4b79d9d0976b98acd2e776f72c6
Jose De La Cruz <jldlcv@gmail.com>
2024-05-13 23:09:25 +0200

    Added parameters to the move function

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ NUMBER_OF_DISKS = 3
 number_of_moves = 2**NUMBER_OF_DISKS - 1
 
 rods = {
-    'A': range(NUMBER_OF_DISKS, 0, -1),
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
     'B': [],
     'C': []
 }
 
-def move():
+def move(n, source, auxiliary, target):
 	print(rods)
+
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
