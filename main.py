3ff82ca1c48bb3386ec348cc6d94f1ab7d1dcc0d
Jose De La Cruz <jldlcv@gmail.com>
2024-05-21 22:00:42 +0200

    Added forward variable

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def move(n, source, auxiliary, target):
 		remainder = (i + 1) % 3
 		if remainder == 1:
 			print(f'Move {i + 1} allowed between {source} and {target}')
+			forward = False
+			if not rods[target]:
+				forward = True
 		elif remainder == 2:
 			print(f'Move {i + 1} allowed between {source} and {auxiliary}')
+		elif remainder == 0:
+			print(f'Move {i + 1} allowed between {auxiliary} and {target}')
 	print(rods)
 
 # Initiate call from source A to target C with auxiliary B
